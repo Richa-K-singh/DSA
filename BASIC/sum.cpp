8174694567b49cb0f641175e74537ead403a8e5d
Richa-K-singh <richaksingh9934@gmail.com>
2024-08-17 16:30:35 +0530

    Create sum.cpp

---
+++ BASIC/sum.cpp
@@ -0,0 +1,26 @@
+// // #include <iostream>
+// // using namespace std ;
+// // int main (){
+
+
+// //     int a = 45;
+// //     int b = 32;
+// //     int c = a+b;
+// //     cout<< c ;
+// // }
+
+
+// #include<iostream>
+// using namespace std ;
+// int main (){
+//     int a, b, c ;
+//     cout<<" Enter first number :";
+//     cin>>a;
+//     cout<<" Enter second number :";
+//     cin>>b ;
+//     c = a+b;
+//     cout<<c;
+// }
+
+#include <iostream>
+using namespace std ;
