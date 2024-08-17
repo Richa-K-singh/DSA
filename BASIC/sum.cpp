f99da8f37506a1caa8de3c5c43ff2b9d69daae8b
Richa-K-singh <richaksingh9934@gmail.com>
2024-08-17 16:46:58 +0530

    Update sum.cpp

--- BASIC/sum.cpp
+++ BASIC/sum.cpp
@@ -10,17 +10,14 @@
 // // }
 
 
-// #include<iostream>
-// using namespace std ;
-// int main (){
-//     int a, b, c ;
-//     cout<<" Enter first number :";
-//     cin>>a;
-//     cout<<" Enter second number :";
-//     cin>>b ;
-//     c = a+b;
-//     cout<<c;
-// }
-
-#include <iostream>
+#include<iostream>
 using namespace std ;
+int main (){
+    int a, b, c ;
+    cout<<" Enter first number :";
+    cin>>a;
+    cout<<" Enter second number :";
+    cin>>b ;
+    c = a+b;
+    cout<<c;
+}
