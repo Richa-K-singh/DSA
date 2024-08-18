c4e3a0e334b6b242f2b53cb5825c796d139258b2
Richa-K-singh <richaksingh9934@gmail.com>
2024-08-18 23:26:29 +0530

    Create sub.cpp

---
+++ practice/sub.cpp
@@ -0,0 +1,31 @@
+// // #include<iostream>
+// // using namespace std ;
+// // int main () {
+// //     int a = 56 ;
+// //     int b = 30 ;
+// //     int c = a-b ;
+// //     cout<< c ;
+// // }
+
+// #include<iostream>
+// using namespace std ;
+// int main () {
+//     int a , b , c ;
+//     cout<<" Enter the first number :" ;
+//     cin>> a;
+//     cout<<" Enter the second number :" ;
+//     cin>> b ;
+//     c= a-b ;
+//     cout << c ;
+
+// }
+
+#include<iostream>
+using namespace std ;
+int main () {
+    int a , b , c ;
+    cout <<" Enter the number :" ;
+    cin>>a>>b ;
+    c = a - b ;
+    cout<< c;
+}
