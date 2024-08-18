6020c8320ff6131ed19c5da6d1a42ef45db209fc
Richa-K-singh <richaksingh9934@gmail.com>
2024-08-18 10:07:34 +0530

    Create equation.cpp

---
+++ BASIC/equation.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std ;
+int main (){
+    int a , b , c ;
+    cout<<" Enter the first number :";
+    cin>>a;
+    cout<<" Enter the second number :";
+    cin>>b;
+    c=a+b;
+    cout<<"a+b="<<c <<endl;
+
+    c = a-b ;
+    cout<<" a-b="<<c <<endl ;
+
+    c=a*b;
+    cout<<" a*b="<<c <<endl;
+
+    c=a/b;
+    cout<<"a/b="<<c ;
+}
