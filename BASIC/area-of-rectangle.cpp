d8390fb7f9adb7fc982e7ba87da68f3faf9ed3fb
Richa-K-singh <richaksingh9934@gmail.com>
2024-08-18 18:48:55 +0530

    Create area-of-rectangle.cpp

---
+++ BASIC/area-of-rectangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std ;
+int main () {
+    int area ,l,b ;
+   
+    cout<<" Enter the length :" ;
+    cin>>l;
+    cout<<" Enter the breadth :" ;
+    cin>>b ;
+     area = l*b ;
+    cout<<area ;
+
+}
