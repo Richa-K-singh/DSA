511402ce37eb3ac386e7c44484bda2ad020fe187
Richa-K-singh <richaksingh9934@gmail.com>
2024-08-18 23:25:48 +0530

    Create div.cpp

---
+++ practice/div.cpp
@@ -0,0 +1,33 @@
+// // #include<iostream>
+// // using namespace std ;
+// // int main (){
+// //     int a = 50 ;
+// //     int b = 5 ;
+// //     int c = a/b ;
+// //     cout<<c ;
+// // }
+
+
+// #include<iostream>
+// using namespace std ;
+// int main () {
+//     int a ,b , c;
+//     cout<<" Enter the first number :" ;
+//     cin>>a;
+//     cout <<" Enter the second number :" ;
+//     cin >> b ;
+//     c = a/b ;
+//     cout << c ;
+
+// }
+
+#include<iostream>
+using namespace std ;
+int main (){
+    int a, b , c ;
+    cout<< " Enter the number :" ;
+    cin >> a >> b ;
+    c = a/b ;
+    cout << c ;
+
+}
