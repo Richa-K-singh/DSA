41c32c76a2876546ef15cc6cff7ca164ad1a67ea
Richa-K-singh <richaksingh9934@gmail.com>
2024-08-18 09:26:49 +0530

    Update sum.cpp

--- practice/sum.cpp
+++ practice/sum.cpp
@@ -1,30 +1,30 @@
+// // // #include<iostream>
+// // // using namespace std ;
+// // // int main (){
+// // //     int a = 45;
+// // //     int b = 32;
+// // //     int c = a+b;
+// // //     cout<<c;
+// // // }
+
 // // #include<iostream>
 // // using namespace std ;
 // // int main (){
-// //     int a = 45;
-// //     int b = 32;
-// //     int c = a+b;
-// //     cout<<c;
+// //     int a, b , c ;
+// //     cout<<" Enter the first number :" ;
+// //     cin >> a ;
+// //     cout <<" Enter the second number :" ;
+// //     cin>> b ;
+// //     c = a+b ;
+// //     cout << c;
 // // }
 
-// #include<iostream>
+// #include <iostream>
 // using namespace std ;
-// int main (){
-//     int a, b , c ;
-//     cout<<" Enter the first number :" ;
-//     cin >> a ;
-//     cout <<" Enter the second number :" ;
-//     cin>> b ;
-//     c = a+b ;
-//     cout << c;
+// int main () {
+//     int a , b , c ;
+//     cout<<" Enter the number :" ;
+//     cin >> a>>b ;
+//     c = a+ b ;
+//     cout<<c;
 // }
-
-#include <iostream>
-using namespace std ;
-int main () {
-    int a , b , c ;
-    cout<<" Enter the number :" ;
-    cin >> a>>b ;
-    c = a+ b ;
-    cout<<c;
-}
