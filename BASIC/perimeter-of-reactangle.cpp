3f3ea3a808ef105ea4fab3eba08772a52870fdd3
Richa-K-singh <richaksingh9934@gmail.com>
2024-08-18 18:49:35 +0530

    Create perimeter-of-reactangle.cpp

---
+++ BASIC/perimeter-of-reactangle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std ;
+int main (){
+    int perimeter , l, b ;
+    cout<< " Enter the length :" ;
+    cin >> l ;
+    cout << " Enter the breadth:" ;
+    cin >> b ;
+    perimeter = 2*(l+b);
+    cout<< perimeter ;
+}
