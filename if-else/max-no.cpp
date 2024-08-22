772a99383f19f22140d599ee41574c9fe3377ff2
Richa-K-singh <richaksingh9934@gmail.com>
2024-08-22 16:23:28 +0530

    Create max-no.cpp

---
+++ if-else/max-no.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std ;
+
+int  main(){
+     int a , b ;
+     cout<<" Enter the number a :";
+     cin>>a;
+     cout<<"Enter the number b :" ;
+     cin >>b;
+
+     if(a>b){
+        cout<<a;
+     }
+     else if (a=b)
+
+     {
+        cout<<"equal";
+
+     }
+     
+
+     else {
+        cout<<b;
+        
+     }
+     
+}
