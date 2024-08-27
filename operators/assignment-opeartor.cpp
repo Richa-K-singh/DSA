4bd65ee7ae44a83d541e09b2c234727593d411c9
Richa-K-singh <richaksingh9934@gmail.com>
2024-08-28 00:30:13 +0530

    Update assignment-opeartor.cpp

--- operators/assignment-opeartor.cpp
+++ operators/assignment-opeartor.cpp
@@ -1,11 +1,45 @@
 #include<iostream>
-using namespace std;
-int main (){
+using namespace std ;
+int main(){
     int a,b;
-    cout<<"Enter a number : ";
+    cout<<"Enter a First number a:";
     cin>>a;
-    cout<<"Enter a number 2: ";
+    cout<<"Enter a second number b:";
     cin>>b;
-    a-=b;
-    cout<<a;
+
+    // a=b;
+    // cout<<"a=b:"<<a<<endl;
+
+    // a+=b;
+    // cout<<"a+=b:"<<a<<endl;
+
+    // a-=b;
+    // cout<<"a-=b:"<<a<<endl;
+
+    // a*=b;
+    // cout<<"a*=b:"<<a<<endl;
+
+    // a/=b;
+    // cout<<"a/b:"<<a<<endl;
+
+    // a%=b;
+    // cout<<"a%=b:"<<a<<endl;
+
+    // a&=b;
+    // cout<<"a&=b:"<<a<<endl;
+
+    // a!=b;
+    // cout<<"a!=b:"<<a<<endl;
+
+    // a>>=b;
+    // cout<<"a>>=b:"<<a<<endl;
+
+    // a^=b;
+    // cout<<"a^=b:"<<a<<endl;
+
+    cout<<"a="<<a;
+    cout<<"b="<<b;
+
+
+
 }
