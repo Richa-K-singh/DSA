70d65f8721745387e32e61e7c628de2f0a61a281
Richa-K-singh <richaksingh9934@gmail.com>
2024-08-28 00:59:48 +0530

    Create add.cpp

---
+++ practice 2.cpp/add.cpp
@@ -0,0 +1,30 @@
+// // #include<iostream>
+// // using namespace std ;
+// // int main (){
+// //     int a=5;
+// //     int b=2;
+// //     int c=a+b;
+// //     cout<<c;
+// // }
+
+// #include<iostream>
+// using namespace std ;
+// int main (){
+//     int a,b,c ;
+//     cout<<"Enter the first number a:";
+//     cin>>a;
+//     cout<<"Enter the second number b:";
+//     cin>>b; 
+//     c=a+b;
+//     cout<<c;
+// }
+
+#include<iostream>
+using namespace std ;
+int main (){
+    int a,b,c;
+    cout<<"Enter the number:";
+    cin>>a>>b;
+    c=a+b;
+    cout<<c;
+}
