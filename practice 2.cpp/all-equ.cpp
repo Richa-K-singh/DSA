e5d6c4db790da89af910983ef3dd10debb6a99e3
Richa-K-singh <richaksingh9934@gmail.com>
2024-08-28 23:37:15 +0530

    Create all-equ.cpp

---
+++ practice 2.cpp/all-equ.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int a,b,c;
+    cout<<"Enter the fitst  number a : ";
+    cin>>a;
+    cout<<"Enter the second number b : ";
+    cin>>b;
+    c=a+b;
+    cout<<c<<endl;
+
+    c=a-b;
+    cout<<c<<endl;
+
+    c=a*b;
+    cout<<c<<endl;
+
+    c=a/b;
+    cout<<c<<endl;
+}    
