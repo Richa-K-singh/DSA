ee6db551430fea3ebadf49e41d718915399560f2
Richa-K-singh <richaksingh9934@gmail.com>
2024-08-29 21:28:47 +0530

    Update nagative-possitive-no.cpp

--- if-else/nagative-possitive-no.cpp
+++ if-else/nagative-possitive-no.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
-using namespace std;
-int main(){
-    int a;
-    cout<<"a";
+using namespace std ;
+int main ()
+{
+    int a ;
+    cout<<"Enter the number a : " ;
     cin>>a;
-    if(a>0)
+    if (a<0){
+        cout<<"negative";
+
+    }
+    else if(a>0){
+        cout<<"possitive";
+    }
+
+    else{
+        cout<<"zero";
+    }
+
+        
+
 
 }
