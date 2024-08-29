db18524f5ab9f2ef2b488cdc7b291c7d1a8c5f09
Richa-K-singh <richaksingh9934@gmail.com>
2024-08-29 17:18:58 +0530

    Create assignment-operator.cpp

---
+++ practice 2.cpp/assignment-operator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+int main (){
+    int a ,b ;
+    cout<<"Enter the first number a : " ;
+    cin>>a;
+    cout<<"Enter the second number b : " ;
+    cin>>b;
+    a=b;
+    a+=b;
+    // a-=b;
+    // a*=b;
+    // a/=b;
+    cout<<"a = "<<a;
+    cout<<"b = "<<b ;
+}
