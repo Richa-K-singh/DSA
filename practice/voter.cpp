521233876ba3a848b551f14695e05a3eebe61b66
Richa-K-singh <richaksingh9934@gmail.com>
2024-08-31 17:47:35 +0530

    Update voter.cpp

--- practice/voter.cpp
+++ practice/voter.cpp
@@ -4,5 +4,11 @@ int main (){
     int age ;
     cout<<" Enter the age : " ;
     cin>>age;
-    if ()
+    if (age>=18){
+        cout<<"Eligible for vote : " ;
+        
+    }
+    else if (age<18){
+        cout<<"Not eligible for vote : ";
+    }
 }
