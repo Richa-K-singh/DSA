c77dae4d8b68262c2005718663e360437174ffce
Richa-K-singh <richaksingh9934@gmail.com>
2024-08-31 17:46:58 +0530

    Create table.cpp

---
+++ for loop/table.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ;
+int main (){
+    int no;// no for number
+     
+    cout<<"Enter a number : ";
+    cin>>no;
+
+    // for (size_t i = 0; i < count; i++)
+    // {
+    //     /* code */
+    // }
+    for(int i)
+}
