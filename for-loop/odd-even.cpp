88801d2c227604bf13087cb032d6ea53eebf60cf
Richa-K-singh <richaksingh9934@gmail.com>
2024-09-07 17:51:11 +0530

    Update odd-even.cpp

--- for-loop/odd-even.cpp
+++ for-loop/odd-even.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 int main(){
     int number;
-    cout<<"Enter a number : ";
+    cout<<"Enter the number:";
     cin>>number;
-    for (int i=1;i<=number;i++){
-        
+    for(int i=1;i<=number;i++){
+        if(i%2==0){
+        cout<<i<<endl;
+        }
     }
 }
