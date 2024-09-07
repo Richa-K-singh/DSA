fab58b71f53e472aaffe2db119005a13e8513cec
Richa-K-singh <richaksingh9934@gmail.com>
2024-09-07 17:50:48 +0530

    Update number.cpp

--- for-loop/number.cpp
+++ for-loop/number.cpp
@@ -2,8 +2,8 @@
 using namespace std ;
 int main (){
     for(int i=0;i<=50;i++){
-        cout<<i<<endl;
-        cout<<" love you mummy ";
+        cout<<i;
+        cout<<" love you mummy "<<endl;
 
     }
 }
