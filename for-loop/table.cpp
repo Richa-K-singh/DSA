cab2d2387bab097029f095dd1e58668ec521b159
Richa-K-singh <richaksingh9934@gmail.com>
2024-09-07 17:52:26 +0530

    Create table.cpp

---
+++ for-loop/table.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<<"Enter a number : ";
+    cin>>number;
+    int i=number*1;
+    cout<<i<<endl;
+    i=number*2;
+    cout<<i<<endl;
+    i=number*3;
+    cout<<i<<endl;
+    i=number*4;
+    cout<<i<<endl;
+    i=number*5;
+    cout<<i<<endl;
+    i=number*6;
+    cout<<i<<endl;
+    i=number*7;
+    cout<<i<<endl;
+    i=number*8;
+    cout<<i<<endl;
+    i=number*9;
+    cout<<i<<endl;
+    i=number*10;
+    cout<<i<<endl;
+}
